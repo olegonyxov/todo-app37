4c2a67c6d45b6d8af806a07b68a21ecfc32ca596
olegonyxov <olegonyxov@gmail.com>
2024-06-27 20:41:08 +0300

    222

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Todos from './components/Todos';
+import { connect } from 'react-redux';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Todo app
+        </p>
+      </header>
+      <div className='content'>
+        <Todos />       
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
